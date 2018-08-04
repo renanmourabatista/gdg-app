87e54b6d78f95812c8b8786ef481774f3435e6df
Renan Moura Batista <renanmourabatista@gmail.com>
2018-08-04 18:37:47 -0400

    alteracao nas rotas

--- src/router/index.js
+++ src/router/index.js
@@ -10,6 +10,10 @@ Vue.use(Router)
 const router = new Router({
     routes: [
         {
+            path:'*',
+            redirect: '/post'
+        },
+        {
             path: '/home',
             name: 'HelloWorld',
             component: HelloWorld,
